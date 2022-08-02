1ce8ea83a79a3e922212cb54fe3e08e29a8b3c3e
joaobragaciandt <joaobraga@ciandt.com>
2022-08-02 09:55:25 -0300

    0. Construindo uma API REST com Spring Boot - SONG API

--- 0x02/construindo_API_REST/com/song/demo/Song.java
+++ 0x02/construindo_API_REST/com/song/demo/Song.java
@@ -54,4 +54,26 @@ public class Song {
     public void setAnoLancamento(String anoLancamento) {
         this.anoLancamento = anoLancamento;
     }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Song song)) return false;
+        if (!super.equals(object)) return false;
+
+        if (getId() != null ? !getId().equals(song.getId()) : song.getId() != null) return false;
+        if (getNome() != null ? !getNome().equals(song.getNome()) : song.getNome() != null) return false;
+        if (getArtista() != null ? !getArtista().equals(song.getArtista()) : song.getArtista() != null) return false;
+        if (getAlbum() != null ? !getAlbum().equals(song.getAlbum()) : song.getAlbum() != null) return false;
+        return getAnoLancamento() != null ? getAnoLancamento().equals(song.getAnoLancamento()) : song.getAnoLancamento() == null;
+    }
+
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (getId() != null ? getId().hashCode() : 0);
+        result = 31 * result + (getNome() != null ? getNome().hashCode() : 0);
+        result = 31 * result + (getArtista() != null ? getArtista().hashCode() : 0);
+        result = 31 * result + (getAlbum() != null ? getAlbum().hashCode() : 0);
+        result = 31 * result + (getAnoLancamento() != null ? getAnoLancamento().hashCode() : 0);
+        return result;
+    }
 }
